a3cb6d42bb23bed00648b8324d727ea405e79f45
soywiz <soywiz@gmail.com>
2019-08-24 01:17:44 +0200

    Try to fix travis on windows

--- travis_win_bintray.bat
+++ travis_win_bintray.bat
@@ -6,9 +6,8 @@ DIR c:\java8
 DIR c:\java8\java-1.8.0-openjdk-1.8.0.191-1.b12.ojdkbuild.windows.x86_64
 SET JAVA_HOME=c:\java8\java-1.8.0-openjdk-1.8.0.191-1.b12.ojdkbuild.windows.x86_64
 
-REM CALL refreshenv
-REM SET GRADLE_ERROR_LEVEL=%errorlevel%
-REM CALL gradlew.bat --stop
-REM exit /b %GRADLE_ERROR_LEVEL%
-
 CALL gradlew.bat --no-daemon -s -i localPublishToBintrayIfRequired
+SET GRADLE_ERROR_LEVEL=%errorlevel%
+CALL gradlew.bat --stop
+taskkill /f /im java.exe
+exit /b %GRADLE_ERROR_LEVEL%
